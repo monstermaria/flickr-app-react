51a8afc975562f3328dec4a9b77fe1d03f9df5e4
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-09-19 13:25:59 +0200

    Added a Header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
     return (
@@ -6,6 +7,7 @@ function App() {
             <header className="App-header">
                 <p>Hi there!</p>
             </header>
+            <Header />
         </div>
     );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return <div>This is the Header component</div>;
+    }
+}
+
+export default Header;
