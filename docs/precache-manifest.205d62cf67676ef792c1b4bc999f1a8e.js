73f61a5fd645606a1016b768e008b263231b73b4
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-09-21 06:32:42 +0200

    New build

---
+++ docs/precache-manifest.205d62cf67676ef792c1b4bc999f1a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2458834294d2793bfaf85c280d390a2",
+    "url": "/flickr-app-react/index.html"
+  },
+  {
+    "revision": "d382e58c523a112f2700",
+    "url": "/flickr-app-react/static/css/main.9c8735fb.chunk.css"
+  },
+  {
+    "revision": "a864e8c80492a28d282b",
+    "url": "/flickr-app-react/static/js/2.c43ff48d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flickr-app-react/static/js/2.c43ff48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d382e58c523a112f2700",
+    "url": "/flickr-app-react/static/js/main.c2707dc7.chunk.js"
+  },
+  {
+    "revision": "131b8e988845a301b524",
+    "url": "/flickr-app-react/static/js/runtime-main.def54811.js"
+  }
+]);
