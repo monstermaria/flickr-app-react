9ec7feb22fc1e7b91e5ba299d4d184ad3bdbdb4a
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-09-21 06:45:01 +0200

    Revert "New build"
    This reverts commit f37f8c24850591b571c1184070d244c88e4bd5e6.

---
+++ docs/precache-manifest.c6b88dfa2aa655e68e6f239cdbe26cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee187efc862918ef265f09d52bb8866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d382e58c523a112f2700",
+    "url": "/static/css/main.9c8735fb.chunk.css"
+  },
+  {
+    "revision": "a864e8c80492a28d282b",
+    "url": "/static/js/2.c43ff48d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c43ff48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d382e58c523a112f2700",
+    "url": "/static/js/main.c2707dc7.chunk.js"
+  },
+  {
+    "revision": "d2121990c56efcda68de",
+    "url": "/static/js/runtime-main.5ba35b08.js"
+  }
+]);
